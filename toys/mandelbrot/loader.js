b07723481d307d3833bf718b9ef93382a2b5a424
Steven L <steven.leibrock@gmail.com>
2022-08-02 13:34:19 -0400

    Updating code to start possibly drawing

--- toys/mandelbrot/loader.js
+++ toys/mandelbrot/loader.js
@@ -3,6 +3,8 @@ request.open('GET', 'mandelbrot.wasm');
 request.responseType = 'arraybuffer';
 request.send();
 
+var wasm_loaded = false;
+
 request.onload = function() {
 	var bytes = request.response;
 	WebAssembly.instantiate(bytes, {
@@ -10,7 +12,24 @@ request.onload = function() {
 			print: (result) => { console.log(`The result is ${result}`); }
 		}
 	}).then(result => {
-		const add = result.instance.exports.add;
-		add(1, 2);
+		const get_pixel_color = result.instance.exports.get_pixel_color;
+		wasm_loaded = true;
 	});
 };
+
+if (!wasm_loaded)
+	throw Error("Wasm failed to load");
+
+// create a running body of code
+(function(){
+	var cnv = window.document.getElementById("canvas");
+	var ctx = cnv.getContext("2d");
+
+	for (var i = 0; i < cnv.width; i++) {
+		for (var j = 0; j < cnv.height; j++) {
+			var iters = get_pixel_color(i, j);
+			ctx.fillStyle = "rgb(" + iters +", "+iters+", "+iters+")";
+			ctx.fillRect(i, j, 1, 1);
+		}
+	}
+})();
--- toys/mandelbrot/mandelbrot.zig
+++ toys/mandelbrot/mandelbrot.zig
@@ -5,6 +5,29 @@ const io = std.io;
 // when importing wasm code, the `print` func must be provided
 extern fn print(i32) void;
 
-export fn add(a: i32, b: i32) void {
-    print(a + b);
+const MAX_ITER: u8 = 255;
+const WIDTH: f32 = 640.0;
+const HEIGHT: f32 = 480.0;
+
+export fn get_pixel_color(px: i32, py: i32) u8 {
+    var iterations: u8 = 0;
+
+    var x0 = @intToFloat(f32, px);
+    var y0 = @intToFloat(f32, py);
+    x0 = x0 / WIDTH;
+    y0 = y0 / HEIGHT;
+    var x: f32 = 0;
+    var y: f32 = 0;
+    var tmp: f32 = 0;
+    var xsquare: f32 = 0;
+    var ysquare: f32 = 0;
+
+    while ((xsquare + ysquare < 4.0) and (iterations < MAX_ITER)) : (iterations += 1) {
+        tmp = xsquare - ysquare + x0;
+        y = 2 * x * y + y0;
+        x = tmp;
+        xsquare = x * x;
+        ysquare = y * y;
+    }
+    return iterations;
 }
